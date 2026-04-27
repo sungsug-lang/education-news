62e0ce08b4b6f547c5044d8b411aee331b01f08a
sungsug-lang <sungsug@gmail.com>
2026-04-27 19:32:59 +0900

    Delete api/news.js

--- api/news.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  res.status(200).json({
-    items: [
-      { title: "교육 뉴스 샘플 1", link: "https://news.google.com" },
-      { title: "교육 뉴스 샘플 2", link: "https://news.google.com" }
-    ]
-  });
-}
